51615819ab7fed05d8d92ede2e48ee1b6aa8ca88
Radu Toader <radu.m.toader@gmail.com>
2017-02-11 10:24:31 +0200

    make container position fixed - with big z index

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ require("!style-loader!css-loader!./style.css");
 var entities = new Entities();
 
 var colors = {
-    reset: ["transparent", "transparent"],
-    black: "181818",
-    red: "E36049",
-    green: "B3CB74",
-    yellow: "FFD080",
-    blue: "7CAFC2",
-    magenta: "7FACCA",
-    cyan: "C3C2EF",
-    lightgrey: "EBE7E3",
-    darkgrey: "6D7891"
+    reset : ["transparent", "transparent"],
+    black : "181818",
+    red : "E36049",
+    green : "B3CB74",
+    yellow : "FFD080",
+    blue : "7CAFC2",
+    magenta : "7FACCA",
+    cyan : "C3C2EF",
+    lightgrey : "EBE7E3",
+    darkgrey : "6D7891"
 };
 ansiHTML.setColors(colors);
 
-function getCurrentScriptSource() {
+function getCurrentScriptSource(){
     // `document.currentScript` is the most accurate way to find the current script,
     // but is not supported in all browsers.
     if (document.currentScript)
@@ -39,7 +39,7 @@ function getCurrentScriptSource() {
 
 
 var urlParts;
-if (typeof __resourceQuery === "string" && __resourceQuery) {
+if (typeof __resourceQuery === "string" && __resourceQuery){
     // If this bundle is inlined, use the resource query to get the correct url.
     urlParts = url.parse(__resourceQuery.substr(1));
 } else {
@@ -53,65 +53,72 @@ if (typeof __resourceQuery === "string" && __resourceQuery) {
 var hot = false;
 var currentHash = "";
 
-if ($('#WebPackBanner-header').length == 0) {
-    $('body').prepend('<div id="WebPackBanner-errors"></div>');
-    $('body').prepend('<div id="WebPackBanner-header">' +
-        '<div id="WebPackBanner-status"></div>' +
-        '</div>');
+if ($('#WDS-Banner-Container').length == 0){
+    $('body').prepend('<div id="WDS-Banner-Container"/>');
+    $('#WDS-Banner-Container')//
+        .html('<div id="WDS-header">' +
+            '<div id="WDS-status"/>' +
+            '</div>' +
+            '<div id="WDS-errors"/>'
+        );
 }
-var status = $("#WebPackBanner-status");
-var $errors = $("#WebPackBanner-errors");
-var header = $("#WebPackBanner-header");
+
+var status = $("#WDS-status");
+var $errors = $("#WDS-errors");
+var header = $("#WDS-header");
 
 
 var onSocketMsg = {
-    invalid: function () {
+    invalid : function (){
         fadeIn();
         status.text("App updated. Recompiling...");
         header.css({
-            'border-color': "#96b5b4"
+            'border-color' : "#96b5b4"
         });
         $errors.hide();
 
     },
-    hash: function (hash) {
+    hash : function (hash){
         currentHash = hash;
     },
-    "still-ok": function () {
+    "still-ok" : function (){
         fadeIn();
         $errors.hide();
         status.text("App ready.");
         header.css({
-            'border-color': "#A3BE8C"
+            'border-color' : "#A3BE8C"
         });
         fadeOut();
     },
-    ok: function () {
+    ok : function (){
         fadeIn();
         $errors.hide();
 
         status.text("App hot update.");
         header.css({
-            'border-color': "#96b5b4"
+            'border-color' : "#96b5b4"
         });
-        setTimeout(function () {
+        setTimeout(function (){
             status.text("App ready.");
             header.css({
-                'border-color': "#A3BE8C"
+                'border-color' : "#A3BE8C"
             });
         }, 1500);
         fadeOut();
 
     },
-    "content-changed": function () {
+    "content-changed" : function (){
         self.location.reload();
-    },
-    warnings: function (warnings) {
+    }
+
+    ,
+    warnings : function (warnings){
         fadeIn();
         $errors.hide();
         status.text("App hot update.");
-    },
-    errors: function (errors) {
+    }
+    ,
+    errors : function (errors){
         fadeIn();
         status.text("Errors while compiling!");
         $errors.html("<span style=\"color: #" +
@@ -119,11 +126,12 @@ var onSocketMsg = {
             "\">Failed to compile.</span><br><br>" +
             ansiHTML(entities.encode(errors[0])));
         header.css({
-            'border-color': "#ebcb8b"
+            'border-color' : "#ebcb8b"
         });
         $errors.show();
-    },
-    close: function () {
+    }
+    ,
+    close : function (){
         fadeIn();
         status.text("Disconnected.");
         $errors.html('<span style="color: #' + colors.yellow + ';margin-top:40px;font-size:1em;display: block">Lost' +
@@ -131,21 +139,21 @@ var onSocketMsg = {
             ' webpack-dev-server.<span><span style="color: #' + colors.yellow + ';margin-top:40px;font-size:1em;display: block">' +
             'Please restart the server to reestablish connection...<span>');
         header.css({
-            'border-color': "#ebcb8b"
+            'border-color' : "#ebcb8b"
         });
         $errors.show();
     }
 };
 var fadeOutTimeout = null;
-function fadeOut() {
-    fadeOutTimeout = setTimeout(function () {
+function fadeOut(){
+    fadeOutTimeout = setTimeout(function (){
         header.fadeOut();
         status.fadeOut();
     }, 3000);
 }
 
-function fadeIn() {
-    if (fadeOutTimeout != null) {
+function fadeIn(){
+    if (fadeOutTimeout != null){
         clearTimeout(fadeOutTimeout);
         header.stop();
         status.stop();
@@ -163,11 +171,11 @@ var protocol = urlParts.protocol;
 
 
 //check ipv4 and ipv6 `all hostname`
-if (hostname === "0.0.0.0" || hostname === "::") {
+if (hostname === "0.0.0.0" || hostname === "::"){
     // why do we need this check?
     // hostname n/a for file protocol (example, when using electron, ionic)
     // see: https://github.com/webpack/webpack-dev-server/pull/384
-    if (self.location.hostname && !!~self.location.protocol.indexOf("http")) {
+    if (self.location.hostname && !!~self.location.protocol.indexOf("http")){
         hostname = self.location.hostname;
     }
 }
@@ -176,16 +184,16 @@ if (hostname === "0.0.0.0" || hostname === "::") {
 // a protocol would result in an invalid URL.
 // When https is used in the app, secure websockets are always necessary
 // because the browser doesn't accept non-secure websockets.
-if (hostname && (self.location.protocol === "https:" || urlParts.hostname === "0.0.0.0")) {
+if (hostname && (self.location.protocol === "https:" || urlParts.hostname === "0.0.0.0")){
     protocol = self.location.protocol;
 }
 
 var socketUrl = url.format({
-    protocol: protocol,
-    auth: urlParts.auth,
-    hostname: hostname,
-    port: (urlParts.port === "0") ? self.location.port : urlParts.port,
-    pathname: urlParts.path == null || urlParts.path === "/" ? "/sockjs-node" : urlParts.path
+    protocol : protocol,
+    auth : urlParts.auth,
+    hostname : hostname,
+    port : (urlParts.port === "0") ? self.location.port : urlParts.port,
+    pathname : urlParts.path == null || urlParts.path === "/" ? "/sockjs-node" : urlParts.path
 });
 
 
